9bc6561e33829879223317094938bab9ee3cafb3
Jan Kus <jankus493@gmail.com>
2018-01-16 19:19:40 +0100

    Minor changes

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     version='0.1',
     packages= ['MyMLL'],
     url='https://github.com/janeq493/MyMLL',
-    download_url='https://github.com/janeq493/MyMLL/archive/0.1.tar.gz'
+    download_url='https://github.com/janeq493/MyMLL/archive/0.1.tar.gz',
+    author = 'Jan Kus',
+    author_email = 'jankus493@gmail.com',
 )
