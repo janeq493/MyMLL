a60446fbdb84bf78cd1de699776de68d22c1db58
Jan Kus <jankus493@gmail.com>
2018-01-16 20:13:47 +0100

    Added training method to neural network

--- MyMLL/NeuralNetwork/neuralNetwork.py
+++ MyMLL/NeuralNetwork/neuralNetwork.py
@@ -1,28 +1,32 @@
 import numpy as np
 from layer import Layer
 
+
 class neuralNetwork:
-    def __init__(self,inputSize):
+    def __init__(self, inputSize):
         self.layerInfo = [inputSize]
         self.Layers = []
 
-    def addLayer(self,size,activationFunc='tanh'):
-        self.Layers.append(Layer(size,self.layerInfo[-1],activationFunc))
+    def addLayer(self, size, activationFunc='tanh'):
+        self.Layers.append(Layer(size, self.layerInfo[-1], activationFunc))
         self.layerInfo.append(size)
 
-    def train()
-        pass
+    def train(self, training, labels, learningRate, batchSize):
+        for t, l in zip(training, labels):
+            self.feedForward(t)
+            self.backProp(l)
+            self.update(batchSize, learningRate)
 
-    def feedForawrd(self,input):
+    def feedForward(self, input):
         for l in self.Layers:
-            input = l.feedForawrd(input)
+            input = l.feedForward(input)
         return input
-    
-    def backProp(self,label):
+
+    def backProp(self, label):
         error = self.Layers[-1].backProp(label=label)
-        for i in range(2,len(self.Layers)):
+        for i in range(2, len(self.Layers)):
             error = self.Layers[-i].backProp(error)
 
-    def update(self,batchSize,learningRate):
+    def update(self, batchSize, learningRate):
         for l in self.Layers:
-            l.update(batchSize,learningRate)
+            l.update(batchSize, learningRate)
