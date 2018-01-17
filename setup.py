67850cafe84506633c7da3ef1ed7d60e000f7923
Jan Kus <jankus493@gmail.com>
2018-01-17 08:30:01 +0100

    Minor fixes

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 setup(
     name='MyMLL',
     version='0.2.1',
-    packages= ['MyMLL'],
+    packages=['MyMLL'],
     url='https://github.com/janeq493/MyMLL',
     download_url='https://github.com/janeq493/MyMLL/archive/0.2.1.tar.gz',
-    author = 'Jan Kus',
-    author_email = 'jankus493@gmail.com',
+    author='Jan Kus',
+    author_email='jankus493@gmail.com',
 )
